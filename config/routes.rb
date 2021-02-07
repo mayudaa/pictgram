6f33e02609071b6f9740416bfcae1ca1874c4852
mayudaa <mayumayu.1014.mayu@gmail.com>
2021-02-07 09:25:20 +0000

    ruby5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  
   root 'pages#index'
   get 'pages/help'
   
   resources :users# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+ get '/login', to: 'sesseions#new'
+ post '/login', to: 'sesseions#create'
+ delete '/logout', to: 'sesseions#destroy'
 end
